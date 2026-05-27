e51d4e9945cc6b82ba57beacbe08ed5a7e41ecd5
Angad198 <angad4082@gmail.com>
2026-05-28 10:00:22 +1200

    Added login system and dashboard

--- app.py
+++ app.py
@@ -1,15 +1,48 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
+app.secret_key = "helpdesksecretkey"
 
+
+# Login Page
 @app.route("/")
-def login():
+def home():
     return render_template("login.html")
 
 
+# Login Validation
+@app.route("/login", methods=["POST"])
+def login_validation():
+
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    # Demo credentials
+    if email == "admin@helpdesk.com" and password == "admin123":
+        session["user"] = email
+        return redirect("/dashboard")
+
+    return render_template(
+        "login.html",
+        error="Invalid Email or Password"
+    )
+
+
+# Dashboard
 @app.route("/dashboard")
 def dashboard():
+
+    if "user" not in session:
+        return redirect("/")
+
     return render_template("dashboard.html")
+
+
+# Logout
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect("/")
 
 
 if __name__ == "__main__":
